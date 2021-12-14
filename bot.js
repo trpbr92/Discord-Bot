eca117e3de30f80e0bcc483e91d5a4492f790641
Trace Brandt <trpbr92@gmail.com>
2021-12-14 08:01:16 -0600

    discord bot online

--- bot.js
+++ bot.js
@@ -1,1 +1,10 @@
-console.log("beep boop!");
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.login(token);
